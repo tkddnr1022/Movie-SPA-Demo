acf35ae87c79a8c6d8b21f91c2aa48c2936c4c1c
tkddnr1022 <tkddnr1022@naver.com>
2024-11-15 04:46:04 +0900

    Feat: add navbar, footer

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,22 @@
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </header>
+  <!--Todo: 로그인 페이지에서 Navbar 숨김-->
+  <Navbar />
   <div class="container">
     <RouterView />
   </div>
+  <Footer />
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
+import Footer from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <style scoped>
 .container {
-  padding: 4rem 2rem 2rem 2rem;
-  height: 100svh;
-  width: 100svw;
-}
-
-header {
-  position: fixed;
+  padding: 0rem 2rem 0rem 2rem;
   width: 100%;
-  padding: 1rem;
-  background-color: #333;
-  color: white;
-}
-
-nav a {
-  color: white;
-  margin-right: 1rem;
-  text-decoration: none;
-}
-
-nav a:hover {
-  text-decoration: underline;
+  background-color: black;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer">
+        <p>&copy; 2024 tkddnr1022. All Rights Reserved.</p>
+        <RouterLink to="/features">Features</RouterLink>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
+
+<style scoped>
+.footer {
+    height: 8vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    background-color: black;
+    color: white;
+    font-size: 0.9rem;
+    opacity: 0.8;
+}
+</style>
