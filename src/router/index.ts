1806917f7245cc10c3962b10a1122b3949964ddd
tkddnr1022 <tkddnr1022@naver.com>
2024-11-26 02:40:30 +0900

    Rename: seperate SignInView from HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,15 +7,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/signin',
+      name: 'signin',
+      component: () => import('../views/SignInView.vue')
     }
   ]
 })
