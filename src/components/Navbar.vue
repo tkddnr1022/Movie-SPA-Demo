a8f0202325976e640922654be6e32483c26ee885
tkddnr1022 <tkddnr1022@naver.com>
2024-11-28 00:39:03 +0900

    Design: make navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,42 @@
 <template>
     <header>
         <nav class="navbar">
+            <div class="menu-toggle" @click="toggleMobileMenu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
             <div class="logo">Movie-SPA-Demo</div>
-            <ul class="nav-links">
-                <li>
-                    <RouterLink to="/">홈</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/popular">인기</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/search">검색</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/wishlist">찜 목록</RouterLink>
-                </li>
-            </ul>
+            <transition name="fade">
+                <ul v-if="isMobileMenuOpen" class="nav-links mobile-menu">
+                    <li @click="closeMobileMenu">
+                        <RouterLink to="/">홈</RouterLink>
+                    </li>
+                    <li @click="closeMobileMenu">
+                        <RouterLink to="/popular">인기</RouterLink>
+                    </li>
+                    <li @click="closeMobileMenu">
+                        <RouterLink to="/search">검색</RouterLink>
+                    </li>
+                    <li @click="closeMobileMenu">
+                        <RouterLink to="/wishlist">찜 목록</RouterLink>
+                    </li>
+                </ul>
+                <ul v-else class="nav-links">
+                    <li>
+                        <RouterLink to="/">홈</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/popular">인기</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/search">검색</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/wishlist">찜 목록</RouterLink>
+                    </li>
+                </ul>
+            </transition>
         </nav>
     </header>
 </template>
@@ -28,6 +49,19 @@ export default {
     components: {
         RouterLink,
     },
+    data() {
+        return {
+            isMobileMenuOpen: false
+        };
+    },
+    methods: {
+        toggleMobileMenu() {
+            this.isMobileMenuOpen = !this.isMobileMenuOpen;
+        },
+        closeMobileMenu() {
+            this.isMobileMenuOpen = false;
+        }
+    }
 };
 </script>
 
@@ -44,10 +78,11 @@ header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1rem 1rem;
+    padding: 1rem;
     color: white;
     font-size: 1rem;
     font-weight: bold;
+    position: relative;
 }
 
 .logo {
@@ -72,5 +107,66 @@ header {
 
 .nav-links a:hover {
     color: #e50914;
+}
+
+.menu-toggle {
+    display: none;
+    flex-direction: column;
+    cursor: pointer;
+}
+
+.bar {
+    width: 25px;
+    height: 3px;
+    background-color: white;
+    margin: 3px 0;
+    transition: 0.4s;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+@media screen and (max-width: 768px) {
+    .menu-toggle {
+        display: flex;
+    }
+
+    .nav-links {
+        display: none;
+    }
+
+    .mobile-menu {
+        display: flex;
+        flex-direction: column;
+        width: 10rem;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: #353535;
+        border-radius: 14px;
+        padding: 0;
+    }
+
+    .nav-links li {
+        margin: 1rem 0;
+        text-align: center;
+    }
+
+    .logo {
+        position: static;
+        transform: none;
+        margin: 0 auto;
+    }
+
+    .navbar {
+        justify-content: flex-start;
+    }
 }
 </style>
