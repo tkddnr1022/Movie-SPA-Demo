7216c994b5b1f3ff3d9edaedd5f52f8f51e8f4ad
tkddnr1022 <tkddnr1022@naver.com>
2024-11-27 20:09:41 +0900

    Feat: add language parameter to movie list api

--- src/scripts/api/get-movie-list.ts
+++ src/scripts/api/get-movie-list.ts
@@ -4,11 +4,13 @@ import { APIBaseURLs } from "../enums/api-base-urls";
 import { getApiKey } from "../utils/storage";
 
 const REGION = import.meta.env.VITE_REGION;
+const LANGUAGE = import.meta.env.VITE_LANGUAGE;
 
 export async function getMovieList(query: string) {
     const params = {
         api_key: getApiKey(),
         region: REGION,
+        language: LANGUAGE,
     }
     try {
         const movies = await axios.get<MovieResponse>(`${APIBaseURLs.MovieList}/${query}`, { params });
